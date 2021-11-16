4a95ff26532a1f8802f461875ab8b8a37ee87cea
conor-c <conor.chung@gmail.com>
2021-11-16 13:57:57 -0500

    finished anonymizer, UUID system leaks email in current solution

--- anonymizer.js
+++ anonymizer.js
@@ -1,62 +1,126 @@
 "use strict";
-// use OLOO
+
 const Account = (function() {
-  let email;
-  let password;
-  let firstName;
-  let lastName;
-  let displayName = (function() {
-    //generate display name
-  })();
+  const DataBase = {};
+
+  function isCorrectPassword(passwordAttempt) {
+    return DataBase[this.randomKey].userPassword === passwordAttempt;
+  }
+
+  const createDisplayName = function() {
+    const displayNameLength = 16;
+    let characters = ['1', '2', '3', '4', '5', '6', '7', 
+                      '8', '9', 'a', 'b', 'c', 'd', 'e',
+                      'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                      'm', 'n', 'o','p', 'q', 'r', 's', 
+                      't', 'u', 'v', 'w', 'x', 'y', 'z']; //can use a string
+    let displayName = '';
+
+    for (let count = 0; count < displayNameLength; count += 1) {
+      let randomIdx = Math.floor(Math.random() * characters.length);
+      displayName += characters[randomIdx];
+    }
+
+    return displayName;
+  };
 
   return {
-    init(emailArg, passwordArg, firstNameArg, lastNameArg) {
-      // set email, password, firstName, lastName
-      // also call anonymizer for display name
-      //    Req: 16 character sequence self generated
-      email = emailArg;
-      password = passwordArg;
-      firstName = firstNameArg;
-      lastName = lastNameArg;
-      // this.displayName = Account.anonymize();
+    init(email, password, firstName, lastName) {
+      if (DataBase[Symbol.for(email)] !== undefined) {
+        console.log("Error: That email already exists.");
+        return;
+      }
+      this.randomKey = Symbol.for(email);
+      DataBase[this.randomKey] = {};
+      DataBase[this.randomKey].userEmail = email;
+      DataBase[this.randomKey].userPassword = password;
+      DataBase[this.randomKey].userFirstName = firstName;
+      DataBase[this.randomKey].userLastName = lastName;
+      this.displayName = createDisplayName();
       return this;
     },
 
     reanonymize(password) {
-      // STUB
-      // Generate a new 16 character sequence and reassigns it to the
-      // display name property if the password provided is valid
-      // return true if successfully re-anonymized
-      // return "Invalid Password" if the password is not valid.
+      if (isCorrectPassword.call(this, password)) {
+        this.displayName = createDisplayName();
+        return true;
+      } else {
+        return "Invalid Password"
+      }
     },
 
     resetPassword(oldPass, newPass) {
-      // if oldPass is true, reassign password property to newPass
-      // return "Invalid Password" if the password provided is not valid
-      // return true if successfully reset
+      if (isCorrectPassword.call(this, oldPass)) {
+        DataBase[this.randomKey].userPassword = newPass;
+        return true;
+      } else {
+        return "Invalid Password";
+      }
     },
 
     firstName(password) {
-      // return the first name of the user if the password is valid
-      // return "Invalid Password" if the password is not valid.
+      if (isCorrectPassword.call(this, password)) {
+        return DataBase[this.randomKey].userFirstName;
+      } else {
+        return "Invalid Password";
+      }
     },
 
     lastName(password) {
-      // return the last name of the user if the password is valid
-      // return "Invalid Password" if the password is not valid
+      if (isCorrectPassword.call(this, password)) {
+        return DataBase[this.randomKey].userLastName;
+      } else {
+        return "Invalid Password";
+      }
     },
 
     email(password) {
-      // return the emailname if the password provided is valid.
-      // return "Invalid Password" if the password is not valid.
+      if (isCorrectPassword.call(this, password)) {
+        return DataBase[this.randomKey].userEmail;
+      } else {
+        return "Invalid Password";
+      }
     },
 
-    displayName() {
-      // return the displayName
-    },
+
   }
 })();
 
+
+
+
+let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+
+// console.log(fooBar.__proto__)
+// console.log(fooBar)
+
+console.log(fooBar.firstName);                     // returns the firstName function
+console.log(fooBar.email);                         // returns the email function
+console.log(fooBar.firstName('123456'));           // logs 'foo'
+console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
+console.log(fooBar.displayName);                   // logs 16 character sequence
+
+console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password';
+console.log(fooBar.resetPassword('123456', 'abc')) // logs true
+
+let displayName = fooBar.displayName;
+console.log(fooBar.reanonymize('abc'));                         // returns true
+console.log(displayName === fooBar.displayName);   // logs false
+
+console.log()
+let bazQux = Object.create(Account).init('baz@qux.com', '123456', 'baz', 'qux');
+console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
+console.log(fooBar.email('abc'));   
+console.log()
+
+console.log(fooBar);
+console.log(bazQux)
+
+let booBar = Object.create(Account).init('foo@bar.com', '1', 'a', 'a');
+console.log(booBar)
+
+console.log(fooBar.firstName('abc'));
+console.log(bazQux.email('123456'));  
 
 
 // class Account {
@@ -116,22 +180,3 @@ const Account = (function() {
 //     // return the displayName
 //   }
 // }
-
-
-let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
-
-console.log(fooBar.__proto__)
-console.log(fooBar)
-console.log(fooBar.email)
-
-// console.log(fooBar.firstName);                     // returns the firstName function
-// console.log(fooBar.email);                         // returns the email function
-// console.log(fooBar.firstName('123456'));           // logs 'foo'
-// console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
-// console.log(fooBar.displayName);                   // logs 16 character sequence
-// console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password';
-// console.log(fooBar.resetPassword('123456', 'abc')) // logs true
-
-// let displayName = fooBar.displayName;
-// fooBar.reanonymize('abc');                         // returns true
-// console.log(displayName === fooBar.displayName);   // logs false
