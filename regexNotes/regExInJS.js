94266f85e313e90cf6b34feef6b80ef35c1d08f1
conor-c <conor.chung@gmail.com>
2021-12-30 14:25:57 -0500

    finished regex book

--- regexNotes/regExInJS.js
+++ regexNotes/regExInJS.js
@@ -65,12 +65,46 @@
 // Write a method that changes the first arithmetic operator (+, -, *, /)
 // in a string to a '?' and returns the resulting string.
 // Don't modify the original string.
-let mysteryMath = function(str) {
-  // return str.replace(/[+-*\/]?/, "?");
-  console.log(str.replace(/[\+\-\*\/]/, '?'));
-}
-mysteryMath('4 + 3 - 5 = 2');
-// -> '4 ? 3 - 5 = 2'
+// let mysteryMath = function(str) {
+//   // return str.replace(/[+-*\/]?/, "?");
+//   console.log(str.replace(/[\+\-\*\/]/, '?'));
+// }
+// mysteryMath('4 + 3 - 5 = 2');
+// // -> '4 ? 3 - 5 = 2'
 
-mysteryMath('(4 * 3 + 2) / 7 - 1 = 1');
+// mysteryMath('(4 * 3 + 2) / 7 - 1 = 1');
 // -> '(4 ? 3 + 2) / 7 - 1 = 1'
+
+// 4. Write a method that changes every arithmetic operator to a ?.
+
+// let mysteriousMath = function(equation) {
+//   console.log(equation.replace(/[\+\-\*\/]/g, '?'));
+// }
+
+// mysteriousMath('4 + 3 - 5 = 2');           // -> '4 ? 3 ? 5 = 2'
+// mysteriousMath('(4 * 3 + 2) / 7 - 1 = 1'); // -> '(4 ? 3 ? 2) ? 7 ? 1 = 1'
+
+// 5. 
+// let danish = function(str) {
+//   console.log(str.replace(/\b(apple|blueberry|cherry)\b/, 'danish'));
+// }
+// danish('An apple a day keeps the doctor away');
+// // -> 'An danish a day keeps the doctor away'
+
+// danish('My favorite is blueberry pie');
+// // -> 'My favorite is danish pie'
+
+// danish('The cherry of my eye');
+// // -> 'The danish of my eye'
+
+// danish('apple. cherry. blueberry.');
+// // -> 'danish. cherry. blueberry.'
+
+// danish('I love pineapple');
+// // -> 'I love pineapple'
+
+let formatDate = function(dateStr) {
+  console.log(dateStr.replace(/^(\d{4})([\-\/])(\d{2})\2(\d{2})$/, '$4.$3.$1'));
+}
+formatDate('2016-06-17'); // -> '17.06.2016'
+formatDate('2016/06/17'); // -> '2016/06/17' (no change)
